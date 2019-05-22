01d9faad4eedef7140f998e6cfd3de4d1ff92778
Oleg Sandro <okuziura@gmail.com>
2019-05-22 17:41:18 +0300

    Controller: JSON & String works, HTML doesn't

--- src/main/java/com/example/controller/TestController.java
+++ src/main/java/com/example/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.example.controller;
 
+import com.example.model.User;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -10,10 +12,30 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class TestController {
 
-    private AtomicLong counter = new AtomicLong();
+    /*private AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/")
     public long printSomething() {
         return counter.incrementAndGet();
+    }*/
+
+    @GetMapping("/profile")
+    public String profile() {
+        return "profile";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "/home";
+    }
+
+    @GetMapping("/login")
+    public ModelAndView login() {
+        User user = new User();
+        user.setLogin("superuser");
+        user.setPassword("124567890");
+        user.setId_role(2);
+        return new ModelAndView("login", "login", user);
+        //return new ModelAndView("login");
     }
 }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Remains of web application</h2>
+</body>
+</html>
